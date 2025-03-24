8be5e869cba005eb3229686268f1e4d48512415b
Aileen Orilloza <amcorilloza@myseneca.ca>
2025-03-23 21:50:02 -0400

    start

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 #AuthorID: amcorilloza
 
+'''
+OPS445 Assignment 1 - Winter 2025
+Program: assignment1.py 
+Author: "Student Name"
+The python code in this file (a1_[Student_id].py) is original work written by
+"Student Name". No code in this file is copied from any other source
+except those provided by the course instructor, including any person,
+textbook, or on-line resource. I have not shared this python script
+with anyone or anything except for submission for grading. I understand
+that the Academic Honesty Policy will be enforced and
+violators will be reported and appropriate action will be taken.
+'''
+
+
+import sys
+
+
+def day_of_week(year: int, month: int, date: int) -> str:
+    "Based on the algorithm by Tomohiko Sakamoto"
+    days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'] 
+    offset = {1:0, 2:3, 3:2, 4:5, 5:0, 6:3, 7:5, 8:1, 9:4, 10:6, 11:2, 12:4}
+    if month < 3:
+        year -= 1
+    num = (year + year//4 - year//100 + year//400 + offset[month] + date) % 7
+    return days[num]
+
+def mon_max(month:int, year:int) -> int:
+    "returns the maximum day for a given month. Includes leap year check"
+    ...
+
+
 def after(date: str) -> str:
     '''
     after() -> date for next day in YYYY-MM-DD string format
@@ -13,30 +44,15 @@ def after(date: str) -> str:
     year = int(str_year)
     month = int(str_month)
     day = int(str_day)
-    lyear = year % 4
-    if lyear == 0:
-        feb_max = 29 # this is a leap year
-    else:
-        feb_max = 28 # this is not a leap year
-
-    lyear = year % 100
-    if lyear == 0:
-        feb_max = 28 # this is not a leap year
-
-    lyear = year % 400
-    if lyear == 0:
-        feb_max = 29 # this is a leap year
-
-    mon_max = { 1:31, 2:feb_max, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
-
     tmp_day = day + 1  # next day
 
-    if tmp_day > mon_max[month]:
-        to_day = tmp_day % mon_max[month]  # if tmp_day > this month's max, reset to 1 
+    if tmp_day > mon_max(month, year):
+        to_day = tmp_day % mon_max(month, year)  # if tmp_day > this month's max, reset to 1 
         tmp_month = month + 1
     else:
         to_day = tmp_day
         tmp_month = month + 0
+
 
     if tmp_month > 12:
         to_month = 1
@@ -45,6 +61,24 @@ def after(date: str) -> str:
         to_month = tmp_month + 0
 
     next_date = f"{year}-{to_month:02}-{to_day:02}"
-
     return next_date
+
+
+def usage():
+    "Print a usage message to the user"
+
+
+def leap_year(year: int) -> bool:
+    "return True if the year is a leap year"
+
+
+def valid_date(date: str) -> bool:
+    "check validity of date and return True if valid"
+
+
+def day_count(start_date: str, stop_date: str) -> int:
+    "Loops through range of dates, and returns number of weekend days"
+
+
+if __name__ == "__main__":
 
